613a0a35bf6a5e801176de0c6ab944e0e30caf1a
Mager2112 <92055047+Mager2112@users.noreply.github.com>
2023-09-08 23:15:10 +0300

    Added new algorithm

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -3,17 +3,29 @@
    All rights reserved lmao
 */
 #include <iostream>
-unsigned long old_grade(int a, int b) // this is uneffective
+unsigned long old_grade(int a, int b) // this is uneffective a^8 = a*a*...*a 8 times, too slow
 {
     int old_a = a;
     for (int i = 1; i < b; i++)
         a *= old_a;
     return a;
 }
+unsigned long new_grade(int a, int b) // more effective a^8 = ((a^2)^2)^2, faster than pow
+{
+    int d = 1;
+    while (b != 0)
+    {
+        if ((b & 1) != 0)
+            d *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return d;
+}
 int main()
 {
     int a, b;
-    std::cout << "Type number and then grade:\n";
+    std::cout << "Type number and then exponent:\n";
     std::cin >> a >> b;
-    std::cout << "Result is:" << old_grade(a, b) << std::endl;
+    std::cout << "Result is:" << new_grade(a, b) << std::endl;
 }
