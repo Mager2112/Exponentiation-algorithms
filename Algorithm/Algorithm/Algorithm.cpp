b3a77a2dab8735c00a9fba3c66fa102cb1a64d7a
Mager2112 <92055047+Mager2112@users.noreply.github.com>
2023-09-08 14:51:09 +0300

    Added files

---
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -0,0 +1,19 @@
+﻿/*
+   Copyright Mager 2023
+   All rights reserved lmao
+*/
+#include <iostream>
+unsigned long old_grade(int a, int b) // this is uneffective
+{
+    int old_a = a;
+    for (int i = 1; i < b; i++)
+        a *= old_a;
+    return a;
+}
+int main()
+{
+    int a, b;
+    std::cout << "Type number and then grade:\n";
+    std::cin >> a >> b;
+    std::cout << "Result is:" << old_grade(a, b) << std::endl;
+}
